c694f95ef988c2ee3dbba6f7973b84c689ffc2aa
o-Hakal-o <hakalsthrope@gmail.com>
2025-12-16 19:45:07 -0500

    Update settings.py

--- django_crud_api/settings.py
+++ django_crud_api/settings.py
@@ -144,6 +144,7 @@ if not DEBUG:
     # Enable the WhiteNoise storage backend, which compresses static files to reduce disk use
     # and renames the files with unique names for each version to support long-term caching
     STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+    
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR , 'media')
 
