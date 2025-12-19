82c5fe241115386a60571f9ef5425e09f525967a
o-Hakal-o <hakalsthrope@gmail.com>
2025-12-19 17:37:55 -0500

    deleting  provisional endpoint
    hehe

--- tasks/urls.py
+++ tasks/urls.py
@@ -27,5 +27,5 @@ urlpatterns = [
     # 4. Redirect t the place where the teachers uppload archives 
     path('api/files/', FileManagementView.as_view(), name='files_manager'),
     path('api/files/download/<int:file_id>/', FileDownloadView.as_view(), name='file_download'),
-    path('api/init-superuser/', SuperuserInitView.as_view(), name='init-superuser'),
+   
 ]
