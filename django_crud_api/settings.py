1cab3ad336c929b4e35b491349e905749d3e98a4
o-Hakal-o <hakalsthrope@gmail.com>
2025-12-20 04:10:54 -0500

    Update settings.py

--- django_crud_api/settings.py
+++ django_crud_api/settings.py
@@ -30,7 +30,8 @@ SECRET_KEY = SECRET_KEY = os.environ.get('SECRET_KEY', default='your secret key'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = 'RENDER' not in os.environ
 
-ALLOWED_HOSTS = ['ittac.onrender.com',
+ALLOWED_HOSTS = [
+    'ittac.onrender.com',
     'localhost',
     '127.0.0.1',]
 
