3f06700c07d743a7dbec8cd819094f773471d6e1
o-Hakal-o <hakalsthrope@gmail.com>
2026-01-23 20:58:49 -0500

    xd

--- tasks/models.py
+++ tasks/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 from django.conf import settings
 from django.contrib.auth.models import AbstractUser
-
+from cloudinary.models import CloudinaryField
 class CustomUser(AbstractUser):
     # Email único y obligatorio
     email = models.EmailField(unique=True, blank=False)
