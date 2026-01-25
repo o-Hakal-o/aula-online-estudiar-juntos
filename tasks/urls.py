eb36b9277b11f5577934b640bbfc126a507e93a7
o-Hakal-o <hakalsthrope@gmail.com>
2026-01-25 02:39:13 -0500

    xd

--- tasks/urls.py
+++ tasks/urls.py
@@ -2,7 +2,7 @@ from django.urls import path , include
 from rest_framework import routers
 from . import views
 from rest_framework.documentation import include_docs_urls
-from .views import LoginView, FileManagementView, FileDownloadView
+from .views import LoginView, FileManagementView
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 from rest_framework_simplejwt.views import (
     TokenObtainPairView,
